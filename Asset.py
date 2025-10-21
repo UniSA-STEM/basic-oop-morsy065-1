7b6a3c75266ab3d2b4634ac7791ae8761571735c
morsy065 <morsy065@mymail.unisa.edu>
2025-10-21 18:47:09 +1030

    Rig Take_damage and repair methods done + Assets class updated

--- Asset.py
+++ Asset.py
@@ -7,11 +7,26 @@ Username: morsy065
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 class Asset:
-    def __init__(self, name, description, encrypted):
+    def __init__(self, name, description):
         self.__name = name
         self.__description = description
-        self.__encrypted = bool
+        self.__encrypted = False
 
+    def get_name(self):
+        return self.__name
+
+    def encrypt(self):
+        if self.__encrypted == False:
+            self.__encrypted = True
+        else:
+            print(f"{self.__name} is already encrypted.")
+
+    def decrypt(self):
+        if self.__encrypted == True:
+            self.__encrypted = False
+        else:
+            print(f"{self.__name} is already decrypted")
+    
     def __str__(self):
         if self.__encrypted == True:
             return f"{self.__name}:{self.__description} [Encrypted]"
--- Rig.py
+++ Rig.py
@@ -6,11 +6,42 @@ ID: 110443633
 Username: morsy065
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from hacker import Hacker
+
 class Rig:
-    def __init__(self, name, damage_counter, broken_state, storage):
+    def __init__(self, name):
         self.__name = name
         self.__damage_counter = 0
         self.__broken_state = False
-        self.__storage = []
+        self.__storage = ["Data Spike", "Data Spike", "Removable Drive"]
         self.__level = 0
         
+    def take_damage(self):
+        if self.__broken_state == True:
+            print(f"{self.__name} is already broken.")
+        
+        self.__damage_counter += 1
+        threshold = self.__level + 2
+
+        if self.__damage_counter < threshold:
+            print(f"{self.__name} took a hit!! Damage: {self.__damage_counter}")
+
+        elif self.__damage_counter == threshold:
+            self.__broken_state = True
+            print(f"{self.__name} is broken!!")
+        else:
+            print("Error")
+
+    def repair(self):
+        if self.__damage_counter == 0:
+            print("No repair is needed!")
+
+        elif "CryptoToken" not in Hacker.__inventory:
+            print("Not enough CryptoTokens avaialble to complete repair!")
+
+        elif "CryptoToken" in Hacker.__inventory:
+            self.__damage_counter = 0
+            self.__broken_state = False
+            print("Rig is restored to original state!")
+        else:
+            print("Error")
