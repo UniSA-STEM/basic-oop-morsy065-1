268102475e9a26032ba09326ef075d20787514a0
morsy065 <morsy065@mymail.unisa.edu>
2025-10-21 22:36:38 +1030

    encrypting and decrypting asset for Hacker class done

--- Hacker.py
+++ Hacker.py
@@ -6,6 +6,7 @@ ID: 110443633
 Username: morsy065
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from rig import Rig
 
 class Hacker:
 
@@ -19,11 +20,56 @@ class Hacker:
         return self.__inventory
 
     def buying_rig(self):
-        if "CryptoToken" in self.__inventory:
+        if self.__rig == True:
+            print("You can only have one rig at a time!")
+
+        elif "CryptoToken" in self.__inventory:
             self.__inventory.remove("CryptoToken")
             self.__rig = True
             print("1 CryptoToken spent! /n Rig activated.")
-        else:
+
+        elif "CryptoToken" not in self.__inventory:
             print("Not enough CryptoTokens to buy a rig.")
 
-    def 
+        else:
+            print("Error")
+
+    def attack(self):
+        if self.__rig == False:
+            print("You have no Rig to attack from!!")
+
+        elif self.__trace_level >= 5:
+            print(f"You are exposed and therefore cannot attack!!")
+
+        elif "Data Spike" not in Rig.__storage:
+            print(f"You have no Data Spikes left!!")
+
+        elif "Data Spike" in Rig.__storage:
+            print(f"Spike launched at {Rig.__name}!!")
+            Rig.take_damage
+            self.__trace_level += 1
+
+        else:
+            print("Error")
+    
+    def encrypt_asset(self, asset_name):
+        if "Security Chip" not in self.__inventory:
+            print("Encryption failed! Not enough Security Chips.")
+        
+        elif asset_name not in self.__inventory:
+            print(f"Encryption failed! Asset '{asset_name}' not found in the inventory.")
+        
+        else:
+            print(f"{asset_name} has been encrypted.")
+
+    def decrypt(self, asset_name):
+        if "Security Chip" not in self.__inventory:
+            print("Decryption failed! Not enough Security Chips.")
+        
+        elif asset_name not in self.__inventory:
+            print(f"Decryption failed! Asset '{asset_name}' not found in the inventory.")
+        
+        else:
+            print(f"{asset_name} has been decrypted.")
+
+    def
